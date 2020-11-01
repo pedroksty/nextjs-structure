09fb816a922d4e7472fb56ad8966672ce1515e15
pedroksty <pedrotrab2132@gmail.com>
2020-11-01 04:50:39 -0300

    Create SEO component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { Container } from '../styles/pages/Home'
+import SEO from '@/components/SEO'
 
 export default function Home() {
   return (
     <Container>
+
+      <SEO
+        title="ReactJS Structure"
+        shouldExludeTitleSuffix
+        image="logo.png"
+      />
 
       <h1>ReactJS Structure</h1>
       <p>A reactJS + Next.JS structure made by Pedro Henrique.</p>
