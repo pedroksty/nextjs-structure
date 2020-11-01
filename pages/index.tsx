34da5c8d748a7a796b7144cdbe3faccd41ef1dd5
pedroksty <pedrotrab2132@gmail.com>
2020-11-01 01:17:40 -0300

    Add eslint and prettier config

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  const abab = 'dale'
+
+  return (
+    <div >
+      <Head>
+        <title>Conecta Talentos</title>
+      </Head>
+
+      <main >
+        <h1>Hello dale</h1>
+      </main>
+
+    </div>
+  )
+}
